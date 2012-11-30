594da5de559ab1fb4ac83eadfff2593968c30b8d
Clint Troxel <ctroxel@visiblemeasures.com>
2012-11-29 19:52:54 -0500

    formatting

--- redweb.rb
+++ redweb.rb
@@ -3,16 +3,16 @@ require 'redis'
 
 before do
   content_type 'text/plain'
-  response.headers["Access-Control-Allow-Origin"] = "*"
+  response.headers['Access-Control-Allow-Origin'] = '*'
 end
 
-# include :key, :value
+# pls include :key, :value
 post '/set' do
   redis = Redis.new
   redis.set(params[:key], params[:value])
 end
 
-# include :key
+# pls include :key
 get '/get' do
   redis = Redis.new
   redis.get(params[:key])
